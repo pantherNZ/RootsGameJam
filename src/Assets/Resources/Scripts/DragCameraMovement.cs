ed945b2408c7bd0945d933cac8d863948cf5943c
Alex Denford <pantherNZ@users.noreply.github.com>
2023-02-18 19:02:02 -0800

    New art, start of new project setup. Grass generation. New background tile. Art test scene. Art demo of new roots using bezier curves.

--- src/Assets/Resources/Scripts/DragCameraMovement.cs
+++ src/Assets/Resources/Scripts/DragCameraMovement.cs
@@ -1,60 +1,48 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
 
 public class DragCameraMovement : MonoBehaviour
 {
-
     [SerializeField]
     PlayerController controller;
-    private Vector3 Origin;
-    private Vector3 Difference;
-    [SerializeField]
-    private Vector3 ResetCamera;
-    private bool drag = false;
-    private float height;
-    public float ScrollSpeed = 2f;
+
+    private Vector3? dragOrigin;
+    private Vector3 startingPos;
     private Camera mainCam;
 
-    void Start(){
+    private void Start()
+    {
         mainCam = Camera.main;
-        height = mainCam.ViewportToWorldPoint( new Vector3( 0.0f, 1.0f, 0.0f ) ).y;
     }
 
-    void Update() {
+    void Update()
+    {
         if( controller.inMenu )
+        {
+            startingPos = controller.transform.position;
             return;
-
-        float currentScreenTop = mainCam.ViewportToWorldPoint( new Vector3( 0.0f, 1.0f, 0.0f ) ).y;
-        float maxSize = currentScreenTop > height ? mainCam.orthographicSize : 10.0f;
-        mainCam.orthographicSize = Mathf.Clamp( mainCam.orthographicSize - Input.mouseScrollDelta.y * ScrollSpeed, 3.0f, maxSize );
-
-        InputPriority.Instance.Request( () => Input.GetMouseButton( 0 ), "rootSelectionUI", -1, () => {
-            if (controller.newRoot != null) return;
-            Difference = (mainCam.ScreenToWorldPoint(Input.mousePosition)) - controller.transform.position;
-            if (drag == false) {
-                drag = true;
-                Origin = mainCam.ScreenToWorldPoint(Input.mousePosition);
-            }
-            if (drag == true) {
-                Vector3 newCameraPos = Origin - Difference;
-                if( mainCam.ViewportToWorldPoint( new Vector3( 0.0f, 1.0f, 0.0f ) ).y > height )
-                {
-                    newCameraPos.y = Mathf.Min( newCameraPos.y, controller.transform.position.y );
-                    Origin = mainCam.ScreenToWorldPoint( Input.mousePosition );
-                }
-                controller.transform.position = newCameraPos;
-            }
-        });
-        if (!Input.GetMouseButton( 0 )){
-            drag = false;
         }
+
+        InputPriority.Instance.Request( () => Input.GetMouseButton( 0 ), "rootSelectionUI", -1, () =>
+        {
+            if( controller.newRoot != null )
+                return;
+
+            if( dragOrigin == null )
+                dragOrigin = mainCam.ScreenToWorldPoint( Input.mousePosition );
+
+            var difference = mainCam.ScreenToWorldPoint( Input.mousePosition ) - controller.transform.position;
+            controller.transform.position = dragOrigin.Value - difference;
+        } );
+
+        if( !Input.GetMouseButton( 0 ) )
+        {
+            dragOrigin = null;
+        }
+
         if( Input.GetKeyDown( KeyCode.Space ) )
         {
-            controller.transform.position = ResetCamera;
-            mainCam.orthographicSize = 5.0f;
+            controller.transform.position = startingPos;
+            dragOrigin = null;
         }
     }
-    
 }
--- src/Assets/Resources/Scripts/LevelGenerator.cs
+++ src/Assets/Resources/Scripts/LevelGenerator.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Linq;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -17,10 +16,11 @@ public class EnvObject
 public class LevelGenerator : MonoBehaviour
 {
     [SerializeField] GameObject dirtTilePrefab;
-    [SerializeField] int dirtTileSize;
+    [SerializeField] Vector2 dirtTileSize;
     [SerializeField] float groundHeightOffset;
     [SerializeField] float timeOfDay;
     [SerializeField] List<EnvObject> envObjects;
+    [SerializeField] List<GameObject> grassObjects;
     [SerializeField] int levelSeed;
 
     private Camera mainCamera;
@@ -34,7 +34,8 @@ public class LevelGenerator : MonoBehaviour
             levelSeed = UnityEngine.Random.Range( 0, int.MaxValue - 1 );
 
         mainCamera = Camera.main;
-        groundHeight = Camera.main.ViewportToWorldPoint( new Vector3( 0.0f, 1.0f, 0.0f ) ).y - dirtTileSize / 2.0f;
+        //groundHeight = Camera.main.ViewportToWorldPoint( new Vector3( 0.0f, 1.0f, 0.0f ) ).y - dirtTileSize.y / 2.0f;
+        groundHeight = -( dirtTilePrefab.transform as RectTransform ).rect.size.y / 2.0f;
         tree = FindObjectOfType<Tree>();
 
         for( int y = -1; y <= 1; y++ )
@@ -54,24 +55,25 @@ public class LevelGenerator : MonoBehaviour
     private void Update()
     {
         var cameraPos = mainCamera.transform.position;
-        var currentTile = new Vector2Int( ( int )( cameraPos.x / dirtTileSize ), ( int )( cameraPos.y / dirtTileSize ) );
+        var currentTile = new Vector2Int( ( int )( cameraPos.x / dirtTileSize.x ), ( int )( cameraPos.y / dirtTileSize.y ) );
         var cameraSize = mainCamera.ViewportToWorldPoint( new Vector3( 1.0f, 1.0f, 0.0f ) ) - cameraPos;
         var bounds = new Rect( cameraPos - cameraSize, cameraSize * 2.0f );
 
         for( int i = -1; i <= 1; i++ )
         {
-            TryConstructTile( new Vector2Int( RoundAwayFromZero( bounds.xMin / dirtTileSize ), currentTile.y + i ) );
-            TryConstructTile( new Vector2Int( RoundAwayFromZero( bounds.xMax / dirtTileSize ), currentTile.y + i ) );
-            TryConstructTile( new Vector2Int( currentTile.x + i, RoundAwayFromZero( ( bounds.yMin - Mathf.Sign( groundHeight ) ) / dirtTileSize ) ) );
-            TryConstructTile( new Vector2Int( currentTile.x + i, RoundAwayFromZero( ( bounds.yMax - Mathf.Sign( groundHeight ) ) / dirtTileSize ) ) );
+            TryConstructTile( new Vector2Int( RoundAwayFromZero( bounds.xMin / dirtTileSize.x ), currentTile.y + i ) );
+            TryConstructTile( new Vector2Int( RoundAwayFromZero( bounds.xMax / dirtTileSize.x ), currentTile.y + i ) );
+            TryConstructTile( new Vector2Int( currentTile.x + i, RoundAwayFromZero( ( bounds.yMin - Mathf.Sign( groundHeight ) ) / dirtTileSize.y ) ) );
+            TryConstructTile( new Vector2Int( currentTile.x + i, RoundAwayFromZero( ( bounds.yMax - Mathf.Sign( groundHeight ) ) / dirtTileSize.y ) ) );
         }
 
-        TryConstructTile( new Vector2Int( RoundAwayFromZero( bounds.xMin / dirtTileSize ), RoundAwayFromZero( ( bounds.yMin - Mathf.Sign( groundHeight ) ) / dirtTileSize ) ) );
-        TryConstructTile( new Vector2Int( RoundAwayFromZero( bounds.xMax / dirtTileSize ), RoundAwayFromZero( ( bounds.yMin - Mathf.Sign( groundHeight ) ) / dirtTileSize ) ) );
-        TryConstructTile( new Vector2Int( RoundAwayFromZero( bounds.xMin / dirtTileSize ), RoundAwayFromZero( ( bounds.yMax - Mathf.Sign( groundHeight ) ) / dirtTileSize ) ) );
-        TryConstructTile( new Vector2Int( RoundAwayFromZero( bounds.xMax / dirtTileSize ), RoundAwayFromZero( ( bounds.yMax - Mathf.Sign( groundHeight ) ) / dirtTileSize ) ) );
+        TryConstructTile( new Vector2Int( RoundAwayFromZero( bounds.xMin / dirtTileSize.x ), RoundAwayFromZero( ( bounds.yMin - Mathf.Sign( groundHeight ) ) / dirtTileSize.y ) ) );
+        TryConstructTile( new Vector2Int( RoundAwayFromZero( bounds.xMax / dirtTileSize.x ), RoundAwayFromZero( ( bounds.yMin - Mathf.Sign( groundHeight ) ) / dirtTileSize.y ) ) );
+        TryConstructTile( new Vector2Int( RoundAwayFromZero( bounds.xMin / dirtTileSize.x ), RoundAwayFromZero( ( bounds.yMax - Mathf.Sign( groundHeight ) ) / dirtTileSize.y ) ) );
+        TryConstructTile( new Vector2Int( RoundAwayFromZero( bounds.xMax / dirtTileSize.x ), RoundAwayFromZero( ( bounds.yMax - Mathf.Sign( groundHeight ) ) / dirtTileSize.y ) ) );
         TryConstructTile( currentTile );
     }
+
     private float RandomFromHash( int lseed )
     {
         float seed = levelSeed * 0.4f + 23;
@@ -79,58 +81,82 @@ public class LevelGenerator : MonoBehaviour
         return xxHashSharp.xxHash.CalculateHash( BitConverter.GetBytes( seed ) ) / ( float )( uint.MaxValue - 1 );
     }
 
-    private void TryConstructTile( Vector2Int pos )
+    private void TryConstructTile( Vector2Int tile )
     {
-        if( tiles.ContainsKey( pos ) )
+        if( tile.y > 0 || tiles.ContainsKey( tile ) || ( tile.x == 0 && tile.y == 0 ) )
             return;
 
-        var newTilePos = new Vector3( ( float )pos.x * dirtTileSize, groundHeight + pos.y * dirtTileSize, 0.0f );
+        var newTilePos = new Vector3( tile.x * dirtTileSize.x, groundHeight + tile.y * dirtTileSize.y, 0.0f );
         var newTile = Instantiate( dirtTilePrefab, newTilePos, Quaternion.identity );
-        tiles.Add( pos, newTile );
-        var seedOffset = pos.x * 13 + pos.y * 5;
+        tiles.Add( tile, newTile );
 
-        if( pos.x != 0 || pos.y != 0 )
+        ConstructEnvObjects( tile, newTile );
+
+        ConstructGrass( tile, newTile );
+
+        RemoveOldTiles();
+    }
+
+    private void ConstructEnvObjects( Vector2Int tile, GameObject newTile )
+    {
+        var seedOffset = tile.x * 13 + tile.y * 5;
+
+        if( tile.y >= 0 || ( tile.x == 0 && tile.y == 0 ) )
+            return;
+
+        foreach( var env in envObjects )
         {
-            foreach( var env in envObjects )
+            seedOffset += 11;
+
+            float random = RandomFromHash( 1211 + seedOffset );
+            if( env.spawnChancePercent < random * 100.0f )
+                continue;
+
+            int numEnvObj = env.numPerTileMin + ( int )( RandomFromHash( 91 + seedOffset ) * ( env.numPerTileMax - env.numPerTileMin ) );
+            List<Pair<Vector2, float>> spawned = new()
             {
-                seedOffset += 11;
+                new Pair<Vector2, float>( tree.transform.position, 3.0f )
+            };
 
-                float random = RandomFromHash( 1211 + seedOffset );
-                if( env.spawnChancePercent < random * 100.0f )
-                    continue;
-
-                int numEnvObj = env.numPerTileMin + ( int )( RandomFromHash( 91 + seedOffset ) * ( env.numPerTileMax - env.numPerTileMin ) );
-                List<Pair<Vector2, float>> spawned = new List<Pair<Vector2, float>>();
-
-                spawned.Add( new Pair<Vector2, float>( tree.transform.position, 3.0f ) );
-
-                for( int i = 0; i < numEnvObj; ++i )
+            for( int i = 0; i < numEnvObj; ++i )
+            {
+                int safety = 0;
+                Vector2? spawnPos = null;
+                do
                 {
-                    int safety = 0;
-                    Vector2? spawnPos = null;
-                    do
-                    {
-                        Vector2 randPos = newTilePos.ToVector2() + new Vector2(
-                            ( RandomFromHash( 99945 + seedOffset + safety * 69 ) - 0.5f ) * dirtTileSize,
-                             ( RandomFromHash( 9345 + seedOffset + safety * 69 ) - 0.5f ) * dirtTileSize );
-                        if( spawned.All( x => Vector2.SqrMagnitude( x.First - randPos ) >= Mathf.Pow( Mathf.Max( env.minDistBetween, x.Second ), 2.0f ) ) )
-                            spawnPos = randPos;
-                    }
-                    while( safety++ < 50 && !spawnPos.HasValue );
+                    Vector2 randPos = newTile.transform.position.ToVector2() + new Vector2(
+                        ( RandomFromHash( 99945 + seedOffset + safety * 69 ) - 0.5f ) * dirtTileSize.x,
+                            ( RandomFromHash( 9345 + seedOffset + safety * 69 ) - 0.5f ) * dirtTileSize.y );
+                    if( spawned.All( x => Vector2.SqrMagnitude( x.First - randPos ) >= Mathf.Pow( Mathf.Max( env.minDistBetween, x.Second ), 2.0f ) ) )
+                        spawnPos = randPos;
+                }
+                while( safety++ < 50 && !spawnPos.HasValue );
 
-                    if( spawnPos.HasValue )
-                    {
-                        spawned.Add( new Pair<Vector2, float>( spawnPos.Value, env.minDistBetween ) );
-                        var envObj = Instantiate( env.obj, spawnPos.Value, Quaternion.Euler( 0.0f, 0.0f, UnityEngine.Random.value * Mathf.PI * 2.0f ) );
-                        envObj.transform.parent = newTile.transform;
-                    }
+                if( spawnPos.HasValue )
+                {
+                    spawned.Add( new Pair<Vector2, float>( spawnPos.Value, env.minDistBetween ) );
+                    var envObj = Instantiate( env.obj, spawnPos.Value, Quaternion.Euler( 0.0f, 0.0f, UnityEngine.Random.value * Mathf.PI * 2.0f ) );
+                    envObj.transform.parent = newTile.transform;
                 }
             }
         }
+    }
 
+    private void ConstructGrass( Vector2Int tile, GameObject newTile )
+    {
+        // Grass height at y == 0, ignore root tile
+        if( tile.y != 0 || tile.x == 0 )
+            return;
+
+        var grass = Instantiate( grassObjects.RandomItem(), new Vector3( newTile.transform.position.x, 0.0f, 0.0f ), Quaternion.identity );
+        grass.transform.parent = newTile.transform.parent;
+    }
+
+    private void RemoveOldTiles( )
+    {
         var currentTile = new Vector2Int(
-            RoundAwayFromZero( mainCamera.transform.position.x / dirtTileSize ),
-            RoundAwayFromZero( mainCamera.transform.position.y / dirtTileSize ) );
+            RoundAwayFromZero( mainCamera.transform.position.x / dirtTileSize.x ),
+            RoundAwayFromZero( mainCamera.transform.position.y / dirtTileSize.y ) );
 
         bool removed = true;
         while( removed )
